5eeb096210cb8e896e60c1f4e51def7f28d8679d
sgso-free <sgso.free@gmail.com>
2022-09-30 22:06:39 -0300

    consigna3

--- Desafio-5/consigna3/index.js
+++ Desafio-5/consigna3/index.js
@@ -1,41 +1,34 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var path = require('path'); 
+const { parsed, error } = require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const productos = require('./routers/productos')
 
 var app = express();
+
+const PORT = process.env.NODE_PORT
+const ENV = process.env.NODE_ENV
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
+ 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', productos) 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http esta escuchando en el puerto ${server.address().port}`)
+  console.log(`http://localhost:${server.address().port}`)
+  console.log(`Environment:${ENV}`)
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+server.on("error", error => console.log(`Error en servidor ${error}`))
