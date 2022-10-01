ca3beb03b5204681835ae19ee61914659fafa2c4
sgso-free <sgso.free@gmail.com>
2022-09-30 21:15:22 -0300

    consigna2

---
+++ Desafio-5/consigna2/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path'); 
+const { parsed, error } = require("dotenv").config();
+
+const productos = require('./routers/productos')
+
+var app = express();
+
+const PORT = process.env.NODE_PORT
+const ENV = process.env.NODE_ENV
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.use('/api', productos) 
+
+// catch 404 and forward to error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http esta escuchando en el puerto ${server.address().port}`)
+  console.log(`http://localhost:${server.address().port}`)
+  console.log(`Environment:${ENV}`)
+})
+
+server.on("error", error => console.log(`Error en servidor ${error}`))
